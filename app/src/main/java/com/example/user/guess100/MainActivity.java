5da2d448b5ec1739821f7d8e58e566aa2152c79e
ChengIHsuan <cosine12621@gmail.com>
2017-11-14 10:47:24 +0800

    Guess

--- app/src/main/java/com/example/user/guess100/MainActivity.java
+++ app/src/main/java/com/example/user/guess100/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,17 +10,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private MainActivity f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViews();
-
+        
+        f.findViews();
     }
 
+    int ans = (int) (Math.random() * 100 + 1);
 
-    private void findViews(){
+    public void findViews(){
         EditText edNum = (EditText) findViewById(R.id.number);
         TextView tvInfo = (TextView) findViewById(R.id.info);
         TextView tvSecret = (TextView) findViewById(R.id.secret);
@@ -27,8 +31,36 @@ public class MainActivity extends AppCompatActivity {
         int num = Integer.parseInt(edNum.getText().toString());
         String s_info = tvInfo.getText().toString();
         String s_secret = tvSecret.getText().toString();
-        
+        tvSecret.setText(ans);
+    }
 
+
+    int min = 0;
+    int max = 100;
+
+    public void send(View view){
+        EditText edNum = (EditText) findViewById(R.id.number);
+        int num = Integer.parseInt(edNum.getText().toString());
+
+
+        if (num > ans) {
+            max=num;
+            new AlertDialog.Builder(this)
+                    .setMessage(min + " to " + num)
+                    .setPositiveButton("OK", null)
+                    .show();
+        } else if (num < ans) {
+            min=num;
+            new AlertDialog.Builder(this)
+                    .setMessage(num + " to " + max)
+                    .setPositiveButton("OK", null)
+                    .show();
+        } else if (num == ans) {
+            new AlertDialog.Builder(this)
+                    .setMessage("BINGO!!!")
+                    .setNegativeButton("關閉", null)
+                    .show();
+        }
     }
 
 }
