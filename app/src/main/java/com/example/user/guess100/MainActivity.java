a5a42e16d424806ecd6cb69c6ece3292c8961900
ChengIHsuan <cosine12621@gmail.com>
2017-11-14 10:07:04 +0800

    findViews

--- app/src/main/java/com/example/user/guess100/MainActivity.java
+++ app/src/main/java/com/example/user/guess100/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.user.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
     }
+
+
+    private void findViews(){
+        EditText edNum = (EditText) findViewById(R.id.number);
+        TextView tvInfo = (TextView) findViewById(R.id.info);
+        TextView tvSecret = (TextView) findViewById(R.id.secret);
+        Button bSend = (Button) findViewById(R.id.send);
+        int num = Integer.parseInt(edNum.getText().toString());
+        String s_info = tvInfo.getText().toString();
+        String s_secret = tvSecret.getText().toString();
+        
+
+    }
+
 }
