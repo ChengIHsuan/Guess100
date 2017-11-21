df5102ed7e8606f433b877e774aab46b4c270326
ChengIHsuan <cosine12621@gmail.com>
2017-11-21 11:40:01 +0800

    1121

--- app/src/main/java/com/example/user/guess100/MainActivity.java
+++ app/src/main/java/com/example/user/guess100/MainActivity.java
@@ -10,31 +10,26 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private MainActivity f;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        f.findViews();
+        findViews();
     }
-
-    int ans = (int) (Math.random() * 100 + 1);
 
     public void findViews(){
         EditText edNum = (EditText) findViewById(R.id.number);
         TextView tvInfo = (TextView) findViewById(R.id.info);
         TextView tvSecret = (TextView) findViewById(R.id.secret);
         Button bSend = (Button) findViewById(R.id.send);
-        int num = Integer.parseInt(edNum.getText().toString());
-        String s_info = tvInfo.getText().toString();
-        String s_secret = tvSecret.getText().toString();
-        tvSecret.setText(ans);
+        String secret = String.valueOf(ans);
+        tvSecret.setText(secret);
+//        int num = Integer.parseInt(edNum.getText().toString());
+//        String s_info = tvInfo.getText().toString();
+//        String s_secret = tvSecret.getText().toString();
     }
 
-
+    int ans = (int) (Math.random() * 100 + 1);
     int min = 0;
     int max = 100;
 
@@ -42,25 +37,34 @@ public class MainActivity extends AppCompatActivity {
         EditText edNum = (EditText) findViewById(R.id.number);
         int num = Integer.parseInt(edNum.getText().toString());
 
+        TextView tvSecret = (TextView) findViewById(R.id.secret);
+        String secret = String.valueOf(ans);
+        tvSecret.setText(secret);
 
-        if (num > ans) {
-            max=num;
+        if (num < max & num > min) {
+            if (num > ans) {
+                max = num;
+                new AlertDialog.Builder(this)
+                        .setMessage(min + " to " + num)
+                        .setPositiveButton("OK", null)
+                        .show();
+            } else if (num < ans) {
+                min = num;
+                new AlertDialog.Builder(this)
+                        .setMessage(num + " to " + max)
+                        .setPositiveButton("OK", null)
+                        .show();
+            } else if (num == ans) {
+                new AlertDialog.Builder(this)
+                        .setMessage("BINGO!!!")
+                        .setNegativeButton("Close", null)
+                        .show();
+            }
+        }else{
             new AlertDialog.Builder(this)
-                    .setMessage(min + " to " + num)
-                    .setPositiveButton("OK", null)
-                    .show();
-        } else if (num < ans) {
-            min=num;
-            new AlertDialog.Builder(this)
-                    .setMessage(num + " to " + max)
-                    .setPositiveButton("OK", null)
-                    .show();
-        } else if (num == ans) {
-            new AlertDialog.Builder(this)
-                    .setMessage("BINGO!!!")
-                    .setNegativeButton("關閉", null)
+                    .setMessage("Please enter the number between " + min + " and " + max)
+                    .setNegativeButton("Close", null)
                     .show();
         }
     }
-
 }
